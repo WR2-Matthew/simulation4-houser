6e9f128a3f087ac30199e6fbfcf6a46aebdb9808
mattostraszewski <mrostraszewski@gmail.com>
2020-07-14 17:21:01 -0400

    Setup folders

---
+++ src/components/House/House.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class House extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+
+    };
+  };
+
+  render() {
+
+    return (
+      <div>
+        House
+      </div>
+    )
+  };
+};
+
+export default House;
---
+++ src/components/Wizard/Wizard.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Wizard extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+
+    };
+  };
+
+  render() {
+
+    return (
+      <div>
+        Wizard
+      </div>
+    )
+  };
+};
+
+export default Wizard;
